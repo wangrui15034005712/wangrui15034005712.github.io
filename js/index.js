e1dee2ef2b44b0f8d292cf791c2200168975c051
wangrui <15034005712@163.com>
2018-10-27 18:32:32 +0800

    index

--- js/index.js
+++ js/index.js
@@ -66,8 +66,8 @@ S.Drawing = (function () {
     },
 
     adjustCanvas: function () {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight-150;
+      canvas.width = window.innerWidth-20;
+      canvas.height = window.innerHeight-170;
     },
 
     clearFrame: function () {
